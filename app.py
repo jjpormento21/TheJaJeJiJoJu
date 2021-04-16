36fe0c83f46c28e8c457106bfb67a6b1434c502e
jjpormento21 <jonahjamespormento21@gmail.com>
2021-04-16 11:14:20 +0800

    added aboutus

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/about-us')
+def index():
+    return render_template('about_us.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
