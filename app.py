07455cc5e8520de4826c9d6c8749b2e4bbfe045b
jjpormento21 <jonahjamespormento21@gmail.com>
2021-05-14 12:47:54 +0800

    completed basic CRUD functions

--- app.py
+++ app.py
@@ -81,5 +81,46 @@ def deleteAll():
     products.delete_many({})
     return redirect(url_for('dashboard'))
 
+@app.route('/admin/view')
+def viewAll():
+    all_products = products.find()
+    return render_template('admin/view.html', products=all_products)
+
+@app.route('/admin/delete_product/<oid>')
+def deleteProduct(oid):
+    products.delete_one({'_id': ObjectId(oid)})
+    return redirect(url_for('viewAll'))
+
+@app.route('/admin/edit_product/<oid>', methods = ['GET', 'POST'])
+def editProductData(oid):
+    currentProduct = products.find_one_or_404({'_id': ObjectId(oid)})
+    if request.method == 'POST':
+        productName = request.form.get('productTitle')
+        productDescription = request.form.get('productDescription')
+        detailedDescr = request.form.get('detailedDescr')
+        productCategory = request.form.get('productCategory')
+        price = request.form.get('price')
+        imgURL = request.form.get('imgURL')
+        stockNumber = request.form.get('stockNumber')
+        isFeatured = request.form.get('customRadio')
+        products.update_one({'_id': ObjectId(oid)},
+        {'$set':
+            {
+                'productName': productName,
+                'productDescription': productDescription,
+                'detailedDescription': detailedDescr,
+                'productCategory': productCategory,
+                'price': float(price),
+                'imageURL': imgURL,
+                'stockNumber': float(stockNumber),
+                'featured': isFeatured,
+                'datePosted': dateToday 
+            }
+        }
+        )
+        return redirect(url_for('viewAll'))
+    else:
+        return render_template('admin/edit_product.html', currentProduct=currentProduct)
+
 if __name__ == "__main__":
     app.run(debug=True)
