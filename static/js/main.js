6f60186a84323424f87025b245869b25c694736c
jjpormento21 <jonahjamespormento21@gmail.com>
2021-05-15 12:07:37 +0800

    added delete item warning to prevent accidental deletion

--- static/js/main.js
+++ static/js/main.js
@@ -2,3 +2,8 @@
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
+
+function setPostToDelete(productId) {
+  document.querySelector(".delete-post-btn").setAttribute("href", '/admin/delete_product/' + productId);
+  console.log(postId);
+}
