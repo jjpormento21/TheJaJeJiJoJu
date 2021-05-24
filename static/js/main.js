53c914b34f9779574403cfc26335dc78e641f639
jjpormento21 <jonahjamespormento21@gmail.com>
2021-05-24 16:26:03 +0800

    cart layout finished

--- static/js/main.js
+++ static/js/main.js
@@ -2,3 +2,16 @@
 $(function () {
     $('[data-toggle="tooltip"]').tooltip()
   });
+
+var cartButton = document.getElementById('cartButton');
+cartButton.addEventListener('click', openCart);
+
+function openCart(){
+  let cart = document.getElementsByClassName('cart')[0];
+  if (cart.style.display === 'none'){
+    cart.style.display = 'block';
+  }
+  else {
+    cart.style.display = 'none';
+  }
+}
