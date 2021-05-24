a379a06218e3c873e3c9c0c1392d4a7ab3562a16
jjpormento21 <jonahjamespormento21@gmail.com>
2021-05-24 13:43:09 +0800

    file restructuring. new init commit

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 from bson.objectid import ObjectId
 import os
+from datetime import datetime
 app = Flask(__name__)
 
 #database stuff
@@ -12,6 +13,7 @@ app.config['MONGO_URI'] = os.getenv('DEVELOPER')
 mongo = PyMongo(app)
 products = mongo.db.products
 
+dateToday = datetime.now()
 #routes
 @app.route('/')
 def index():
@@ -39,6 +41,86 @@ def about_us():
 @app.errorhandler(404)
 def notFound(e):
     return render_template('404.html')
+
+#Admin Page Routes
+@app.route('/admin/dashboard')
+def dashboard():
+    return render_template('admin/dashboard.html')
+
+@app.route('/admin/add_data', methods=['GET', 'POST'])
+def addData():
+    all_products = products.find()
+    if request.method == 'POST':
+        productName = request.form.get('productTitle')
+        productDescription = request.form.get('productDescription')
+        detailedDescr = request.form.get('detailedDescr')
+        productCategory = request.form.get('productCategory')
+        price = request.form.get('price')
+        imgURL = request.form.get('imgURL')
+        stockNumber = request.form.get('stockNumber')
+        isFeatured = request.form.get('customRadio')
+        products.insert_one(
+            {
+                'productName': productName,
+                'productDescription': productDescription,
+                'detailedDescription': detailedDescr,
+                'productCategory': productCategory,
+                'price': float(price),
+                'imageURL': imgURL,
+                'stockNumber': int(stockNumber),
+                'featured': isFeatured,
+                'datePosted': dateToday 
+            }
+        )
+        return redirect(url_for('dashboard'))
+    else:
+        return render_template('admin/add_data.html')
+
+@app.route('/admin/delete_data_all')
+def deleteAll():
+    products.delete_many({})
+    return redirect(url_for('dashboard'))
+
+@app.route('/admin/view')
+def viewAll():
+    all_products = products.find()
+    return render_template('admin/view.html', products=all_products)
+
+@app.route('/admin/delete_product/<oid>')
+def deleteProduct(oid):
+    products.delete_one({'_id': ObjectId(oid)})
+    return redirect(url_for('viewAll'))
+
+@app.route('/admin/edit_product/<oid>', methods = ['GET', 'POST'])
+def editProductData(oid):
+    currentProduct = products.find_one_or_404({'_id': ObjectId(oid)})
+    if request.method == 'POST':
+        productName = request.form.get('productTitle')
+        productDescription = request.form.get('productDescription')
+        detailedDescr = request.form.get('detailedDescr')
+        productCategory = request.form.get('productCategory')
+        price = request.form.get('price')
+        imgURL = request.form.get('imgURL')
+        stockNumber = request.form.get('stockNumber')
+        isFeatured = request.form.get('customRadio')
+        products.update_one({'_id': ObjectId(oid)},
+        {'$set':
+            {
+                'productName': productName,
+                'productDescription': productDescription,
+                'detailedDescription': detailedDescr,
+                'productCategory': productCategory,
+                'price': float(price),
+                'imageURL': imgURL,
+                'stockNumber': float(stockNumber),
+                'featured': isFeatured,
+                'datePosted': dateToday 
+            }
+        }
+        )
+        return redirect(url_for('viewAll'))
+    else:
+        return render_template('admin/edit_product.html', currentProduct=currentProduct)
 
 if __name__ == "__main__":
     app.run(debug=True)
