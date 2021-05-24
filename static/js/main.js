ae9b041af0797dc49f5e80f881a35d19752be47a
jjpormento21 <jonahjamespormento21@gmail.com>
2021-05-24 12:54:22 +0800

    fixed conflicts on JS file

--- static/js/main.js
+++ static/js/main.js
@@ -1,14 +1,4 @@
 //Tooltip
 $(function () {
-<<<<<<< HEAD
     $('[data-toggle="tooltip"]').tooltip()
   });
-=======
-  $('[data-toggle="tooltip"]').tooltip()
-})
-
-function setPostToDelete(productId) {
-  document.querySelector(".delete-post-btn").setAttribute("href", '/admin/delete_product/' + productId);
-  console.log(postId);
-}
->>>>>>> about-us
