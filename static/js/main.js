77514a570e45375b7415ece9c6e14d131338995a
jjpormento21 <jonahjamespormento21@gmail.com>
2021-05-26 17:59:58 +0800

    finished step 3: payment method

--- static/js/main.js
+++ static/js/main.js
@@ -1,16 +1,37 @@
 //Tooltip
 $(function () {
-    $('[data-toggle="tooltip"]').tooltip()
-  });
+  $('[data-toggle="tooltip"]').tooltip()
+});
 
+// Input Fields: Checkout
 let checkbox1 = document.getElementById('customCheck1');
 checkbox1.addEventListener('click', sameAddress);
 
-function sameAddress(){
+let codInput = document.getElementById('COD');
+codInput.addEventListener('click', hideCardPaymentMethod);
+
+let creditInput = document.getElementById('creditCard')
+creditInput.addEventListener('click', showCardPaymentMethod);
+
+let debitInput = document.getElementById('debitCard')
+debitInput.addEventListener('click', showCardPaymentMethod);
+
+function showCardPaymentMethod(){
+  let cardPaymentSection = document.getElementById('payCard');
+  cardPaymentSection.style.display = 'block';
+}
+function sameAddress() {
   let billingAddInput = document.getElementById('billingAdd');
   let billingAddress = billingAddInput.value;
   let shippingAddInput = document.getElementById('shipAdd');
-  if (checkbox1.checked == true){
+  if (checkbox1.checked == true) {
     shippingAddInput.value = billingAddress;
+  }
+}
+
+function hideCardPaymentMethod() {
+  let cardPaymentSection = document.getElementById('payCard');
+  if (codInput.checked == true) {
+    cardPaymentSection.style.display = 'none';
   }
 }
