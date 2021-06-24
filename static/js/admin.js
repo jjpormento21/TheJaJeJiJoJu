28090b781fe2a03051a83a753e2e7290872a6296
jjpormento21 <jonahjamespormento21@gmail.com>
2021-06-24 17:53:21 +0800

    init filter search

--- static/js/admin.js
+++ static/js/admin.js
@@ -12,3 +12,23 @@ function setRecordToDelete(itemId) {
     document.querySelector(".delete-post-btn").setAttribute("href", '/admin/delete_record/' + itemId);
     console.log(itemId);
 }
+
+var tableRecord = document.querySelector('.records');
+var search = document.querySelector('.searchBar');
+search.addEventListener('keyup', filterItems);
+
+function filterItems(e) {
+    // Convert to lowercase
+    let text = e.target.value.toLowerCase();
+    let tableItems = tableRecord.querySelectorAll('tr');
+    Array.from(tableItems).forEach(function (item){
+        var itemName = item.innerText;
+        if (itemName.toLowerCase().indexOf(text) !=-1){
+            item.style.display = '';
+        }
+        else{
+            item.style.display = 'none';
+        }
+    })
+
+}
